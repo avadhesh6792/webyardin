819b2aef913de1cc55865b83239c3d017d81d2d9
Avadhesh Bhatt <avadheshbhatt92@gmail.com>
2016-10-13 20:53:44 +0530

    worked in footer and heading css

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="footer">
     <div class="row">
         <div class="col-lg-12">
-            &copy;  2014 yourdomain.com | Design by: <a target="_blank" style="color:#fff;" href="http://binarytheme.com">www.binarytheme.com</a>
+            Copyright All right reserved @Yardingapp 2016-17
         </div>
     </div>
 </div>
